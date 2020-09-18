b9e24423e3df8d8f4a6453ad8367ee4f1b7eeab9
Clinton van Ry <clinton.vanry@gmail.com>
2020-09-18 18:41:56 +0100

    this was tough

--- assignment_cvtColor/main.cpp
+++ assignment_cvtColor/main.cpp
@@ -5,12 +5,16 @@ using namespace cv;
 
 Mat convertBGRtoGray(Mat image);
 Mat convertBGRtoHSV(Mat image);
+void printDiff(Mat img1, Mat img2);
 
 int main()
 {
 
     auto img = imread("sample.jpg");
     auto img2 = imread("sample01.jpg");
+
+
+
 
     std::cout << typeToString(img.type()) << std::endl;
     namedWindow("sample", WINDOW_NORMAL);
@@ -25,18 +29,24 @@ int main()
 
     Mat diffGray;
     absdiff(gray,gray_cv,diffGray);
+    printDiff(gray, gray_cv);
 
     imshow("Gray::Result from custom function", gray);
     imshow("Gray::Result from OpenCV function", gray_cv);
     imshow("Gray::Difference", diffGray);
     waitKey(0);
 
-    Mat hsv = convertBGRtoHSV(img2);
+
+    //Mat imgTest = Mat(1,1, CV_8UC3, Scalar(47,88,129));
+    //https://stackoverflow.com/questions/44896166/converting-a-single-bgr-color-with-cvtcolor-and-using-the-result-to-extract-that
+    Mat imgTest = Mat(1,1, CV_8UC3, Scalar(126,105,98));
+    Mat hsv = convertBGRtoHSV(imgTest);
     Mat hsv_cv;
-    cvtColor(img2,hsv_cv,COLOR_BGR2HSV);
+    cvtColor(imgTest,hsv_cv,COLOR_BGR2HSV);
 
     Mat diffHSV;
     absdiff(hsv,hsv_cv,diffHSV);
+    printDiff(hsv, hsv_cv);
 
     imshow("HSV::Result from custom function", hsv);
     imshow("HSV::Result from OpenCV function", hsv_cv);
@@ -76,12 +86,100 @@ Mat convertBGRtoGray(Mat image)
 
 }
 
+
+
+
 Mat convertBGRtoHSV(Mat image)
 {
+    Mat hsv_Image = image.clone();
+
+    // compute the max, min and difference
+    const int BLUE = 0, GREEN =1, RED = 2;
+    for(int row = 0 ; row < image.rows; row++)
+    {
+        for(int col=0; col < image.cols; col++)
+        {
+            Vec3b pixel = image.at<Vec3b>(row,col);
+            float blue = pixel[BLUE]/255.0f, red = pixel[RED]/255.0f, green = pixel[GREEN]/255.0f;
+
+            float value = std::max<float>(std::max<float>(blue,green),red);
+            float minPixel = std::min<float>(std::min<float>(blue,green),red);
+            float diffPixel = value - minPixel;
+
+            float hue= -1, saturation = -1;
+
+            // saturation calculation
+            if(value == 0.0f)
+            {
+                saturation = 0.0f;
+            }
+            else
+            {
+                saturation = diffPixel / value;
+            }
+
+            // hue calculation
+            if (value == red)
+            {
+                hue = 60 * ((green - blue) / diffPixel) ;
+            }
+            else if (value == green)
+            {
+                hue = 120 + 60 * ((blue - red) / diffPixel);
+            }
+            else{
+                hue = 240 + 60 * ((red - green) / diffPixel);
+            }
+
+
+            // final adjustment
+            if(hue < 0){
+                hue+=360.0f;
+            }
+
+            // get to 8bit image
+            value *= 255.0f;
+            saturation *=255.0f;
+            hue /=2.0f;
+
+            Vec3b hsvPixel;
+            hsvPixel[0] = static_cast<int>(  std::round(hue));
+            hsvPixel[1] = static_cast<int>(  std::round(saturation));
+            hsvPixel[2] = static_cast<int>(  std::round(value));
+
+            hsv_Image.at<Vec3b>(row,col) = hsvPixel;
+
+        }
+    }
+
+
     Mat hsv_cv;
     cvtColor(image,hsv_cv,COLOR_BGR2HSV);
-
+    std::cout << "conversion from opencv" << std::endl;
     std::cout << hsv_cv << std::endl;
+    //imshow("hsv_cv", hsv_cv);
+    //waitKey(0);
 
-    return hsv_cv;
+    std::cout << "manual conversion" << std::endl;
+    std::cout << hsv_Image << std::endl;
+    //imshow("hsv_Image", hsv_Image);
+    //waitKey(0);
+
+
+    return hsv_Image;
+}
+void printDiff(Mat img1, Mat img2) {
+    Mat imgDiff;
+    absdiff(img1, img2, imgDiff);
+    for (int row = 0; row < imgDiff.rows; row++)
+    {
+        for (int col = 0; col < imgDiff.cols; col++)
+        {
+            if(imgDiff.at<uchar>(row,col) != 0){
+                std::cout<< "difference at (" << row << " , " << col << ")" << std::endl;
+            }
+        }
+    }
+
+
 }
