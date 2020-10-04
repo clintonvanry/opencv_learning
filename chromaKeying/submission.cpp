6cf68cca2a773fb757bd5dea15bf28f0a18943f8
Clinton van Ry <clinton.vanry@gmail.com>
2020-10-04 15:32:57 +0100

    initial commit

---
+++ chromaKeying/submission.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+
+
+// https://github.com/richardjpurcell/chromaKeyer/blob/master/chromaKeyer/submission.cpp
+// https://github.com/Kawaboongawa/OpenCV-Chroma-Key/blob/master/src/main.cpp
+// https://github.com/Zhovtukhin/chroma-keying-opencv/blob/master/chroma_keying.py
+// https://github.com/paulocoliveira/computer-vision-color-replacement/blob/master/color_replacement.py
+// https://github.com/rmoham05/Chroma_Key/blob/master/Chorma_key.ipynb
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
