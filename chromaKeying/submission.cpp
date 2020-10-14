6b7ecd2d8da9ae5b3310e6a750a165ef2b19261f
Clinton van Ry <clinton.vanry@gmail.com>
2020-10-14 12:29:30 +0100

    small cleanup

--- chromaKeying/submission.cpp
+++ chromaKeying/submission.cpp
@@ -9,13 +9,6 @@
 using namespace cv;
 // https://medium.com/fnplus/blue-or-green-screen-effect-with-open-cv-chroma-keying-94d4a6ab2743
 
-
-// https://github.com/richardjpurcell/chromaKeyer/blob/master/chromaKeyer/submission.cpp
-// https://github.com/Kawaboongawa/OpenCV-Chroma-Key/blob/master/src/main.cpp
-// https://github.com/Zhovtukhin/chroma-keying-opencv/blob/master/chroma_keying.py
-// https://github.com/paulocoliveira/computer-vision-color-replacement/blob/master/color_replacement.py
-// https://github.com/rmoham05/Chroma_Key/blob/master/Chorma_key.ipynb
-
 /*
  * Controls: You can build a simple interface using HighGUI. It should contain the following parts.
 
@@ -72,8 +65,7 @@ int main() {
     backgroundImage = imread("background.jpg",IMREAD_COLOR);
 
     std::string videoFilename = "greenscreen-asteroid.mp4";
-
-
+    
     // check to see if the video can be used and is in the directory to be read.
     VideoCapture videoCapture(videoFilename);
     if(!videoCapture.isOpened())
@@ -344,8 +336,8 @@ void PrintVideoProperties(const VideoCapture& videoCapture)
 }
 void RecordVideo()
 {
-    std::string videoFilename = "greenscreen-asteroid.mp4";
-    std::string outputVideoFilename = "output.mp4";
+    std::string videoFilename = "./greenscreen-asteroid.mp4";
+    std::string outputVideoFilename = "./output.mp4";
 
     // check to see if the video can be used and is in the directory to be read.
     VideoCapture videoReader(videoFilename);
@@ -364,8 +356,6 @@ void RecordVideo()
         BlendImageWithChromaLevels(false);
 
         // save to disk
-        //Mat rgbFrame;
-        //cvtColor(blendedImage, rgbFrame, COLOR_HSV2RGB);
         videoWriter.write(blendedImage);
         imshow("VideoFrame", blendedImage);
         waitKey(25);
