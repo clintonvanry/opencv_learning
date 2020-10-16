3d4f1802be1eba3dbafc7a2dc8f35c5b3a54b961
Clinton van Ry <clinton.vanry@gmail.com>
2020-10-16 18:03:56 +0100

    step 1

--- assignment_imageAlignment/main.cpp
+++ assignment_imageAlignment/main.cpp
@@ -6,6 +6,32 @@
 using namespace cv;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Image alignment" << std::endl;
+
+    Mat img = imread("emir.jpg",IMREAD_GRAYSCALE);
+    // Find the width and height of the color image
+    Size sz = img.size();
+    int height = sz.height / 3;
+    int width = sz.width;
+
+    std::cout << sz << std::endl;
+
+    // Extract the three channels from the gray scale image
+    std::vector<Mat>channels;
+    channels.push_back(img( Rect(0, 0,         width, height)));
+    channels.push_back(img( Rect(0, height,    width, height)));
+    channels.push_back(img( Rect(0, 2*height,  width, height)));
+
+    Mat blue = channels[0];
+    Mat green = channels[1];
+    Mat red = channels[2];
+
+    imshow("original",img);
+    imshow("blue", blue);
+    imshow("green", green);
+    imshow("red", red);
+
+    waitKey(0);
+
     return 0;
 }
